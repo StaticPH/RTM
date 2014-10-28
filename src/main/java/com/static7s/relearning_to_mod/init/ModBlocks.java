dc810b3f7eb0d85fcbf54e1b0f890334f27815d5
static7s <static7s@users.noreply.github.com>
2014-10-28 13:31:30 -0400

    Revived my old StickySoulSand and BlindingSand mods

--- src/main/java/com/static7s/relearning_to_mod/init/ModBlocks.java
+++ src/main/java/com/static7s/relearning_to_mod/init/ModBlocks.java
@@ -4,13 +4,18 @@ import com.static7s.relearning_to_mod.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
 import com.static7s.relearning_to_mod.block.BlockRTM;
 import com.static7s.relearning_to_mod.block.BlockGoldenAppleBlock;
+import com.static7s.relearning_to_mod.block.BlockStickySoulSand;
+import com.static7s.relearning_to_mod.block.BlockBlindingSand;
 
 public class ModBlocks
 {
     public static final BlockRTM goldenAppleBlock = new BlockGoldenAppleBlock();
-
+    public static final BlockRTM stickySoulSand = new BlockStickySoulSand();
+    public static final BlockRTM blindingSand = new BlockBlindingSand();
     public static void init()
     {
         GameRegistry.registerBlock(goldenAppleBlock, "goldenAppleBlock");
+        GameRegistry.registerBlock(stickySoulSand, "stickySoulSand");
+        GameRegistry.registerBlock(blindingSand, "blindingSand");
     }
 }
