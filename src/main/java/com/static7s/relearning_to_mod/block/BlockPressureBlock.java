da4bacdec2659116a0bad6d3fe6271805e3af7db
static7s <static7s@users.noreply.github.com>
2014-12-23 16:06:05 -0500

    -Added a full block sized redstone wire -New Redstone Wire Block currently has texture of stone block, if anyone wants to help me by enabling it to mimic the textures of the blocks around it, that would be much appreciated. -    For some reason breaking the Redstone Wire Block drops redstone dust...I'll see about fixing that

---
+++ src/main/java/com/static7s/relearning_to_mod/block/BlockPressureBlock.java
@@ -0,0 +1,199 @@
+package com.static7s.relearning_to_mod.block;
+
+import com.static7s.relearning_to_mod.creativetab.CreativeTabRTM;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockBasePressurePlate;
+import net.minecraft.block.BlockFence;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
+
+
+
+import java.util.Random;
+
+public class BlockPressureBlock extends BlockRTM
+{
+    public BlockPressureBlock()
+    {
+        super(Material.rock);
+        this.setCreativeTab(CreativeTabRTM.RTM_TAB);
+        this.setTickRandomly(true);
+        this.setBlockName("pressureBlock");
+        this.setBlockTextureName("pressureBlock");
+        this.setStepSound(null);
+    }
+    /**
+     * Returns a bounding box from the pool of bounding boxes (this means this box can change after the pool has been
+     * cleared to be reused)
+     */
+    public AxisAlignedBB getCollisionBoundingBoxFromPool(World p_149668_1_, int p_149668_2_, int p_149668_3_, int p_149668_4_) {
+        float f = 0.125F;
+        return AxisAlignedBB.getAABBPool().getAABB((double) p_149668_2_, (double) p_149668_3_, (double) p_149668_4_, (double) (p_149668_2_ + 1), (double) ((float) (p_149668_3_ + 1) - f), (double) (p_149668_4_ + 1));
+    }
+    /**
+     * How many world ticks before ticking
+     */
+    @Override
+    public int tickRate(World p_149738_1_)
+    {
+        return 20;
+    }
+   @Override
+    public int isProvidingWeakPower(IBlockAccess block, int x, int y, int z, int side)
+    {
+//       return this.func_150060_c(block.getBlockMetadata(x, p_149709_3_, z));
+    return block.getBlockMetadata(x, x, z) == 1 ? 15 : 0;
+    }
+    @Override
+    public int isProvidingStrongPower(IBlockAccess block, int x, int y, int z, int side)
+    {
+//        return side == 1 ? this.func_150060_c(block.getBlockMetadata(x, y, z)) : 0;
+        if (block.getBlockMetadata(x, y, z) == 1 && side == 1) {
+            return 15;
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * Can this block provide power. Only wire currently seems to have this change based on its state.
+     */
+    public boolean canProvidePower()
+    {
+        return true;
+    }
+    /**
+     * Returns the mobility information of the block, 0 = free, 1 = can't push but can move over, 2 = total immobility
+     * and stop pistons
+     */
+    public int getMobilityFlag()
+    {
+        return 1;
+    }
+    /**
+     * Lets the block know when one of its neighbor changes. Doesn't know which neighbor changed (coordinates passed are
+     * their own) Args: x, y, z, neighbor Block
+     */
+    public void onNeighborBlockChange(World world, int x, int y, int z, Block p_149695_5_)
+    {
+        boolean flag = false;
+
+        if (!World.doesBlockHaveSolidTopSurface(world, x, y - 1, z) && !BlockFence.func_149825_a(world.getBlock(x, y - 1, z)))
+        {
+            flag = true;
+        }
+
+        if (flag)
+        {
+            this.dropBlockAsItem(world, x, y, z, world.getBlockMetadata(x, y, z), 0);
+            world.setBlockToAir(x, y, z);
+        }
+    }
+
+    /**
+     * Ticks the block if it's been scheduled
+     */
+    @Override
+    public void updateTick(World world, int x, int y, int z, Random rand)
+    {
+        if (!world.isRemote)
+        {
+//            int l = this.func_150060_c(world.getBlockMetadata(x, y, z));
+//
+//            if (l > 0)
+//            {
+//                this.func_150062_a(world, x, y, z, l);
+//            }
+            if (world.getBlockMetadata(x, y, z) != 1) {
+                this.setBlockState(world, x, y, z);
+            }
+        }
+    }
+    public void func_150062_a(World p_150062_1_, int p_150062_2_, int p_150062_3_, int p_150062_4_, int p_150062_5_)
+    {
+//        int i1 = this.func_150065_e(p_150062_1_, p_150062_2_, p_150062_3_, p_150062_4_);
+//        boolean flag = i1 > 0;
+
+//        if (p_150062_5_ != i1)
+//        {
+//            p_150062_1_.setBlockMetadataWithNotify(p_150062_2_, p_150062_3_, p_150062_4_, this.func_150066_d(i1), 2);
+//            this.func_150064_a_(p_150062_1_, p_150062_2_, p_150062_3_, p_150062_4_);
+//            p_150062_1_.markBlockRangeForRenderUpdate(p_150062_2_, p_150062_3_, p_150062_4_, p_150062_2_, p_150062_3_, p_150062_4_);
+//        }
+
+//        if (flag)
+//        {
+            p_150062_1_.scheduleBlockUpdate(p_150062_2_, p_150062_3_, p_150062_4_, this, this.tickRate(p_150062_1_));
+//        }
+    }
+    /**
+     * Triggered whenever an entity collides with this block (enters into the block). Args: world, x, y, z, entity
+     */
+@Override
+    public void onEntityCollidedWithBlock(World world, int x, int y, int z, Entity par5Entity) {
+        par5Entity.motionX *= 0.4D;
+        par5Entity.motionZ *= 0.4D;
+        if ((par5Entity instanceof EntityPlayer)) {
+            if (world.getBlockMetadata(x, y, z) != 1) {
+                this.setBlockState(world, x, y, z);
+            }
+//            int l = this.func_150060_c(world.getBlockMetadata(x, y, z));
+//
+//            if (l == 0)
+//            {
+//                this.func_150062_a(world, x, y, z, l);
+//            }
+        }
+    }
+    public void setBlockState(World world, int x, int y, int z) {
+        boolean alreadyPressed = world.getBlockMetadata(x, y, z) == 1;
+        boolean currentlyPressed = false;
+        float i = 0.125F;
+
+        if (currentlyPressed && !alreadyPressed) {
+            world.setBlockMetadataWithNotify(x, y, z, 1, 2);
+            world.notifyBlocksOfNeighborChange(x, y, z, this);
+            world.notifyBlocksOfNeighborChange(x, y - 1, z, this);
+        }
+        if (!currentlyPressed && alreadyPressed) {
+            world.setBlockMetadataWithNotify(x, y, z, 0, 2);
+            world.notifyBlocksOfNeighborChange(x, y, z, this);
+            world.notifyBlocksOfNeighborChange(x, y - 1, z, this);
+        }
+        if (currentlyPressed) {
+            world.scheduleBlockUpdate(x, y, z, this, this.tickRate(world));
+        }
+    }
+    public void setBlockBoundsBasedOnState(IBlockAccess block, int x, int y, int z) {
+        boolean on = block.getBlockMetadata(x, y, z) == 1;
+        float height = 1.0F;
+        if (on) {
+           height = 1.0F;
+        }
+        float dim1 = 1.0F;
+        float dim2 = 1.0F;
+        float dim3 = 1.0F;
+        float dim4 = 1.0F;
+        if (block.getBlock(x, y, z - 1) == block) {
+            dim2 = 1.0F;
+        }
+        if (block.getBlock(x, y, z + 1) == block) {
+            dim4 = 1.0F;
+        }
+        if (block.getBlock(x - 1, y, z) == block) {
+            dim1 = 1.0F;
+        }
+        if (block.getBlock(x + 1, y, z) == block) {
+            dim3 = 1.0F;
+        }
+        setBlockBounds(dim1, 1.0F, dim2, dim3, height, dim4);
+    }
+
+}
---
+++ src/main/java/com/static7s/relearning_to_mod/block/BlockPressureSensor.java
@@ -0,0 +1,196 @@
+package com.static7s.relearning_to_mod.block;
+
+import com.static7s.relearning_to_mod.creativetab.CreativeTabRTM;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockBasePressurePlate;
+import net.minecraft.block.BlockFence;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.World;
+
+
+
+
+import java.util.Iterator;
+import java.util.List;
+import java.lang.String;
+
+public class BlockPressureSensor extends BlockBasePressurePlate {
+    public BlockPressureSensor.Sensitivity field_150069_a;
+
+
+    public BlockPressureSensor(String p_i45418_1_, Material p_i45418_2_, BlockPressureSensor.Sensitivity p_i45418_3_) {
+        super(p_i45418_1_, p_i45418_2_);
+        this.field_150069_a = p_i45418_3_;
+        this.setCreativeTab(CreativeTabRTM.RTM_TAB);
+//        this.setTickRandomly(true);
+        this.setBlockName("pressureSensor");
+        this.setBlockTextureName("pressureSensor");
+        this.setStepSound(null);
+        /**
+         * Sets the bounds of the block.  minX, minY, minZ, maxX, maxY, maxZ
+         */
+        this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
+    }
+    public int func_150066_d(int p_150066_1_) {
+        return p_150066_1_ > 0 ? 1 : 0;
+    }
+
+    public int func_150060_c(int p_150060_1_) {
+        return p_150060_1_ == 1 ? 15 : 0;
+    }
+
+    public int func_150065_e(World p_150065_1_, int p_150065_2_, int p_150065_3_, int p_150065_4_) {
+        List list = null;
+
+        if (this.field_150069_a == BlockPressureSensor.Sensitivity.everything) {
+            list = p_150065_1_.getEntitiesWithinAABBExcludingEntity((Entity) null, this.func_150061_a(p_150065_2_, p_150065_3_, p_150065_4_));
+        }
+
+        if (this.field_150069_a == BlockPressureSensor.Sensitivity.mobs) {
+            list = p_150065_1_.getEntitiesWithinAABB(EntityLivingBase.class, this.func_150061_a(p_150065_2_, p_150065_3_, p_150065_4_));
+        }
+
+        if (this.field_150069_a == BlockPressureSensor.Sensitivity.players) {
+            list = p_150065_1_.getEntitiesWithinAABB(EntityPlayer.class, this.func_150061_a(p_150065_2_, p_150065_3_, p_150065_4_));
+        }
+
+        if (list != null && !list.isEmpty()) {
+            Iterator iterator = list.iterator();
+
+            while (iterator.hasNext()) {
+                Entity entity = (Entity) iterator.next();
+
+                if (!entity.doesEntityNotTriggerPressurePlate()) {
+                    return 15;
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    public static enum Sensitivity {
+        everything,
+        mobs,
+        players;
+
+        private static final String __OBFID = "CL_00000290";
+    }
+
+    /**
+     * Is this block (a) opaque and (b) a full 1m cube?  This determines whether or not to render the shared face of two
+     * adjacent blocks and also whether the player can attach torches, redstone wire, etc to this block.
+     */
+    @Override
+    public boolean isOpaqueCube() {
+        return true;
+    }
+
+    /**
+     * If this block doesn't render as an ordinary block it will return False (examples: signs, buttons, stairs, etc)
+     */
+    @Override
+    public boolean renderAsNormalBlock() {
+        return true;
+    }
+    /**
+     * Sets the block's bounds for rendering it as an item
+     */
+    @Override
+    public void setBlockBoundsForItemRender()
+    {
+        float f = 0.5F;
+        float f1 = 0.125F;
+        float f2 = 0.5F;
+        this.setBlockBounds(0.5F - f, 0.5F - f1, 0.5F - f2, 0.5F + f, 0.5F + f1, 0.5F + f2);
+    }
+
+    /**
+     * Returns a bounding box from the pool of bounding boxes (this means this box can change after the pool has been
+     * cleared to be reused)
+     */
+    @Override
+    public AxisAlignedBB getCollisionBoundingBoxFromPool(World p_149668_1_, int p_149668_2_, int p_149668_3_, int p_149668_4_)
+    {
+        return null;
+    }
+    @Override
+    public void breakBlock(World p_149749_1_, int p_149749_2_, int p_149749_3_, int p_149749_4_, Block p_149749_5_, int p_149749_6_)
+    {
+        if (this.func_150060_c(p_149749_6_) > 0)
+        {
+            this.func_150064_a_(p_149749_1_, p_149749_2_, p_149749_3_, p_149749_4_);
+        }
+
+        super.breakBlock(p_149749_1_, p_149749_2_, p_149749_3_, p_149749_4_, p_149749_5_, p_149749_6_);
+    }
+    /**
+     * Checks to see if its valid to put this block at the specified coordinates. Args: world, x, y, z
+     */
+    public boolean canPlaceBlockAt(World p_149742_1_, int p_149742_2_, int p_149742_3_, int p_149742_4_)
+    {
+        return World.doesBlockHaveSolidTopSurface(p_149742_1_, p_149742_2_, p_149742_3_ - 1, p_149742_4_) || BlockFence.func_149825_a(p_149742_1_.getBlock(p_149742_2_, p_149742_3_ - 1, p_149742_4_));
+    }
+    /**
+     * Sets the bounds of the block.  minX, minY, minZ, maxX, maxY, maxZ
+     */
+    protected void func_150063_b(int p_150063_1_)
+    {
+        boolean flag = this.func_150060_c(p_150063_1_) > 0;
+        //float f = 0.0625F;
+
+        if (flag)
+        {
+            this.setBlockBounds(1.0F, 1.0F, 1.0F, 1.0F, 1.0F, 1.0F);
+        }
+        else
+        {
+            this.setBlockBounds(1.0F, 1.0F, 1.0F, 1.0F , 1.0F, 1.0F );
+        }
+    }
+
+
+    protected void func_150062_a(World p_150062_1_, int p_150062_2_, int p_150062_3_, int p_150062_4_, int p_150062_5_) {
+        int i1 = this.func_150065_e(p_150062_1_, p_150062_2_, p_150062_3_, p_150062_4_);
+        //boolean flag = p_150062_5_ > 0;
+        boolean flag1 = i1 > 0;
+
+        if (p_150062_5_ != i1) {
+            p_150062_1_.setBlockMetadataWithNotify(p_150062_2_, p_150062_3_, p_150062_4_, this.func_150066_d(i1), 2);
+            this.func_150064_a_(p_150062_1_, p_150062_2_, p_150062_3_, p_150062_4_);
+            p_150062_1_.markBlockRangeForRenderUpdate(p_150062_2_, p_150062_3_, p_150062_4_, p_150062_2_, p_150062_3_, p_150062_4_);
+        }
+
+        /*if (!flag1 && flag)
+        {
+            p_150062_1_.playSoundEffect((double)p_150062_2_ + 0.5D, (double)p_150062_3_ + 0.1D, (double)p_150062_4_ + 0.5D, "step.stone", 0.3F, 0.5F);
+        }
+        else if (flag1 && !flag)
+        {
+            p_150062_1_.playSoundEffect((double)p_150062_2_ + 0.5D, (double)p_150062_3_ + 0.1D, (double)p_150062_4_ + 0.5D, "step.stone", 0.3F, 0.6F);
+        }*/
+
+        if (flag1) {
+            p_150062_1_.scheduleBlockUpdate(p_150062_2_, p_150062_3_, p_150062_4_, this, this.tickRate(p_150062_1_));
+        }
+    }
+    protected AxisAlignedBB func_150061_a(int p_150061_1_, int p_150061_2_, int p_150061_3_)
+    {
+        float f = 0.125F;
+        return AxisAlignedBB.getAABBPool().getAABB((double)((float)p_150061_1_ + f), (double)p_150061_2_, (double)((float)p_150061_3_ + f), (double)((float)(p_150061_1_ + 1) - f), (double)p_150061_2_ + 0.25D, (double)((float)(p_150061_3_ + 1) - f));
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons (IIconRegister par1IconRegister)
+    {
+        this.blockIcon = par1IconRegister.registerIcon("relearning_to_mod:PressureSensor");
+    }
+    }
