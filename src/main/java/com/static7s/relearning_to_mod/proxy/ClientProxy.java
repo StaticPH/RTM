9b95e11d0cf4c51db69a21ff9a881e69fe223148
Static7s <static7s@users.noreply.github.com>
2015-07-23 14:36:44 -0400

    Tinkered with configs a little. Nothing really significant

--- src/main/java/com/static7s/relearning_to_mod/proxy/ClientProxy.java
+++ src/main/java/com/static7s/relearning_to_mod/proxy/ClientProxy.java
@@ -1,15 +1,33 @@
 package com.static7s.relearning_to_mod.proxy;
 
 import com.static7s.relearning_to_mod.entity.EntityLlamaSpawnerWIP;
+//import com.static7s.relearning_to_mod.handler.EntityJoinHandler;
 import com.static7s.relearning_to_mod.init.ModItems;
 import com.static7s.relearning_to_mod.renderer.RendererLlamaSpawnerWIP;
+//import com.static7s.relearning_to_mod.tweaks.entity.passive.ChickenTweaks;
 import cpw.mods.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.common.MinecraftForge;
 
 public class ClientProxy extends CommonProxy{
     @Override
-    public void initRenderers() {
+    public ClientProxy getClientProxy(){
+        return this;
+    }
+
+    @Override
+    public void initRenders (){
 //        RenderingRegistry.registerEntityRenderingHandler(EntityMankiniCapsule.class, new RendererMankiniCapsule(ModItems.itemMankiniCapsule));
 //        RenderingRegistry.registerEntityRenderingHandler(EntityMankiniCreeper.class, new RendererMankiniCreeper(new ModelCreeper(), 0.5F));
         RenderingRegistry.registerEntityRenderingHandler(EntityLlamaSpawnerWIP.class, new RendererLlamaSpawnerWIP(ModItems.llamaRing));
     }
+    @Override
+    public void registerKeyBindings(){
+
+    }
+//    @Override
+//    public void registerClientEvents()
+//    {
+//        MinecraftForge.EVENT_BUS.register(new EntityJoinHandler());
+//    }
+
 }
--- src/main/java/com/static7s/relearning_to_mod/proxy/ServerProxy.java
+++ src/main/java/com/static7s/relearning_to_mod/proxy/ServerProxy.java
@@ -1,5 +1,22 @@
 package com.static7s.relearning_to_mod.proxy;
 
 
+
 public class ServerProxy extends CommonProxy{
+
+    @Override
+    public ClientProxy getClientProxy(){
+        return null;
+    }
+    @Override
+    public void initRenders (){
+
+    }
+    @Override
+    public void registerKeyBindings(){
+
+    }
+//    @Override
+//    public void registerClientEvents(){
+//    }
 }
